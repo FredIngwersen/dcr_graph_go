82b0b310508fa04ee29ddbf5029b3b00a28a6ebb
Frederik <frederik.ingwersen@gmail.com>
2019-11-27 16:22:10 +0100

    Go XML works and data structures

--- DCRgraph.go
+++ DCRgraph.go
@@ -1,46 +1,108 @@
 package main
 
-import "fmt"
-import "strconv"
+import (
+    "fmt"
+    "encoding/xml"
+    "os"
+    "strings"
+)
 
-type marking struct {
-    executed, included, pending []string
+type DCR struct {
+    Events Event `xml:"events"`
+    Constraints Constraint `xml:"constraints"`
+    Markings Marking `xml:"markings"`
 }
 
-type DCRgraph struct{
+type Event struct {
+    Names []string `xml:"event"`
+}
+
+type Constraint struct {
+    Conditions []string `xml:"conditions>condition"`
+    Responses []string `xml:"responses>response"`
+    Corresponses []string `xml:"corresponses>corresponse"`
+    Excludes []string `xml:"excludes>exclude"`
+    Includes []string `xml:"includes>include"`
+    Milestones []string `xml:"milestones>milestone"`
+    Spawns []string `xml:"spawns>spawn"`
+}
+
+type Marking struct {
+    Executed []string `xml:"executed>event"`
+    Included []string `xml:"included>event"`
+    Pending []string `xml:"pending>event"`
+}
+
+type Constraint_map struct {
+     constraint_map map[string][]string
+}
+
+type DCR_graph struct{
     nodes []string
 
-    conditions_for map[string][]string
-    milestones_for map[string][]string
-    responses_to   map[string][]string
-    excludes_to    map[string][]string
-    includes_to    map[string][]string
+    conditions_for Constraint_map
+    milestones_for Constraint_map
+    responses_to   Constraint_map
+    excludes_to    Constraint_map
+    includes_to    Constraint_map
 
-    markings marking
+    marking Marking
 }
 
-func create_DCRgraph(number_of_nodes int) DCRgraph {
-    g:= DCRgraph{nodes:make([]string, number_of_nodes)}
-    g.markings = marking{executed:make([]string, number_of_nodes),
-        included:make([]string, number_of_nodes),
-        pending:make([]string, number_of_nodes)}
-    return g
+func create_DCR_graph(file string) DCR_graph{
+
+    dcr_graph := new(DCR_graph)
+    dcr := decode_xml(file)
+
+    dcr_graph.nodes = dcr.Events.Names
+    dcr_graph.marking = dcr.Markings
+    dcr_graph.conditions_for = extract_constraints(dcr.Constraints.Conditions)
+
+    return *dcr_graph
 }
 
-func populated_graph(graph DCRgraph, number_of_nodes int){
-    for i := 0; i < number_of_nodes; i++{
-        graph.nodes[i] = strconv.Itoa(i)
+func extract_constraints(constraints []string) Constraint_map{
+    c_map := new(Constraint_map)
+    c_map.constraint_map = make(map[string][]string)
+
+    for i:=0; i < len(constraints); i++ {
+        var splits = strings.Split(constraints[i], ":")
+
+        if _, ok := c_map.constraint_map[splits[0]]; ok {
+            c_map.constraint_map[splits[0]] = append(c_map.constraint_map[splits[0]], splits[1])
+        } else {
+            c_map.constraint_map[splits[0]] = []string{splits[1]}
+        }
     }
+
+    return *c_map
 }
 
-func printGraph(graph DCRgraph){
-    fmt.Println(graph.nodes)
+func decode_xml(file string) DCR {
+    xmlFile, err := os.Open(file)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fmt.Println("Successfully Opened %s", file)
+
+    // defer the closing of our xmlFile so that we can parse it later on
+    defer xmlFile.Close()
+
+    dcr := new(DCR)
+
+    decoded := xml.NewDecoder(xmlFile)
+    err = decoded.Decode(dcr)
+
+    if err != nil {
+       fmt.Println(err)
+    }
+    return *dcr
 }
 
 func main(){
-    number_of_nodes := 10
-    graph1 := create_DCRgraph(number_of_nodes)
-    populated_graph(graph1, number_of_nodes)
-    printGraph(graph1)
+    var filename string = "graph.xml"
+    graph := create_DCR_graph(filename)
+    fmt.Println(graph.conditions_for)
 }
-
